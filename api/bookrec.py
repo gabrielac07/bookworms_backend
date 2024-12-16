60fc5174d22c9ded9bb0e7d673b8ffcf49640729
173433633 <adtband12@gmail.com>
2024-12-16 15:23:50 -0800

    connected bookrec.py api to main.py

--- api/bookrec.py
+++ api/bookrec.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify, request #Import request to handle query parameters
 from flask_cors import CORS  # Import CORS
+from flask import Blueprint
+from flask_restful import Api, Resource # used for REST API building
 import sqlite3
 import random
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+
+bookrec_api = Blueprint('bookrec_api', __name__,
+                   url_prefix='/api')
+
+# API docs https://flask-restful.readthedocs.io/en/latest/
+api = Api(bookrec_api)
 
 # Helper function to get a random book from the database filtered by genre
 def get_random_book(genre=None):
