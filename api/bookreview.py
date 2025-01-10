deeeb25fbed9d01f2839a004e9dc15504286726d
SouminiK97 <souminikandula@gmail.com>
2025-01-09 18:08:11 -0800

    book review

--- api/bookreview.py
+++ api/bookreview.py
@@ -1,29 +1,31 @@
 from flask import Blueprint, jsonify
-from flask_cors import CORS
 from flask_restful import Api
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from model.librarydb import Book  
 import random
-from model.librarydb import books  
+from __init__ import app, db 
 
+# Define Blueprint and Api
 bookreview_api = Blueprint('bookreview_api', __name__, url_prefix='/api')
 api = Api(bookreview_api)
 
-CORS(bookreview_api)
-
-engine = create_engine('sqlite:///library.db', echo=True) 
-Session = sessionmaker(bind=engine)
-session = Session()
-
 # Function to get a random book
 def get_random_book():
-    books_query = session.query(books).all()  
-    return random.choice(books_query) if books_query else None
+    try:
+        # Query all books using SQLAlchemy ORM and random choice
+        books_query = Book.query.all()  # Fetch all books from the database
+        if books_query:
+            return random.choice(books_query)  # Pick a random book if books are available
+        else:
+            return None  # Return None if no books are found
+    except Exception as e:
+        print(f"Error while fetching random book: {e}")
+        return None
 
-@bookreview_api.route('/random_book', methods=['GET'])  
+@bookreview_api.route('/random_book', methods=['GET'])
 def random_book():
     book = get_random_book()
     if book:
+        # If a random book is found, return its details as JSON
         return jsonify({
             'title': book.title,
             'author': book.author,
@@ -32,4 +34,5 @@ def random_book():
             'image_cover': book.cover_image_url
         })
     else:
+        # If no book is found, return an error message with 404 status code
         return jsonify({'error': 'No books found'}), 404
