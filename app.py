dd7fb19ec75c2fcc259de859b41451b22152ce19
maryamabdul-aziz <maryam.d39c@gmail.com>
2025-01-22 21:04:26 -0800

    register app.py blueprint

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Blueprint
+from flask_restful import Api
 from flask_cors import CORS
+from __init__ import app
 
+app_api = Blueprint('app_api', __name__, url_prefix='/api/app')
+api = Api(app_api)
 
 @app.route('/api/avika')
 def get_avika():
